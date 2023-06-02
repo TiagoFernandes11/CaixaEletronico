99fabf4b7c1db79499a01d681cfeaa0fa05d80a9
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-06-02 20:39:48 -0300

    Update Caixa.java

--- entities/Caixa.java
+++ entities/Caixa.java
@@ -1,10 +1,76 @@
 package entities;
 
+import java.util.Scanner;
+
 public class Caixa {
     private Conta[] contas = new Conta[5];
+    private int tamanhoContas = 0;
     private Conta contaCliente;
+    private boolean logado = false;
 
     public Caixa() {
+    }
+
+    
+
+    public void logar(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Email:");
+        String email = sc.nextLine();
+        System.out.println("Senha: ");
+        String senha = sc.nextLine();
+        autenticar(email, senha);
+    }
+
+    public void autenticar(String email, String senha){
+        for(int i = 0; i < this.contas.length; i++){
+            if(email.equals(contas[i].getEmail()) && senha.equals(contas[i].getSenha())){
+                this.contaCliente = contas[i];
+                this.logado = true;
+                System.out.println("Logado com sucesso!");
+                return;
+            }
+        }
+    }
+
+    public void depositar(){
+        if(!this.logado){
+            System.out.println("Você precisa estar logado! ");
+            return;
+        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Qual valor do depósito: ");
+        Double valor = sc.nextDouble();
+        if(valor <= 0){
+            System.out.println("Valor precisa ser positivo e maior que zero!");
+            return;
+        }
+        contaCliente.setSaldo(contaCliente.getSaldo() + valor);
+        System.out.println("Depósito feito com sucesso!\nSeu saldo é de R$" + this.contaCliente.getSaldo());
+    }
+
+    public Conta criarConta(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Email:");
+        String email = sc.nextLine();
+        System.out.println("Senha: ");
+        String senha = sc.nextLine();
+        return new Conta(email, senha, 0);
+    }
+
+    public void cadastrarConta(){
+        if(this.contas.length == tamanhoContas){
+            aumentaCapacidade();
+        }
+        this.contas[tamanhoContas++] = criarConta();
+    }
+
+    private void aumentaCapacidade(){
+        Conta[] temp = new Conta[this.contas.length + 1];
+        for(int i = 0; i < this.contas.length; i++){
+            temp[i] = contas[i];
+        }
+        this.contas = temp;
     }
 
 }
